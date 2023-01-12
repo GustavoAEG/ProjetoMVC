17c3e8a7527a27eeaa2e7913a073c4943523f444
Gustavo Enricco Goncalves Alexandre <gustavoalexandre@employer.com.br>
2023-01-11 22:19:06 -0300

    first commit

---
+++ Controllers/ContatoController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProjetoMVC.Controllers;
+public class ContatoController : Controller{
+        
+public IActionResult Index()
+{
+    return View();
+}
+
+}
