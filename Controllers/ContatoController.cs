a26da7ab1aff8867d1e61834e6c272f41147170d
Gustavo Enricco Goncalves Alexandre <gustavoalexandre@employer.com.br>
2023-01-12 21:43:38 -0300

    #007 - Alteracoes no Index

--- Controllers/ContatoController.cs
+++ Controllers/ContatoController.cs
@@ -1,11 +1,19 @@
 using Microsoft.AspNetCore.Mvc;
+using ProjetoMVC.Context;
 
 namespace ProjetoMVC.Controllers;
 public class ContatoController : Controller{
-        
+
+        private readonly AgendaContext _context;
+
+        public ContatoController(AgendaContext context){
+
+            _context = context;
+        }
 public IActionResult Index()
 {
-    return View();
+    var contatos = _context.Contatos.ToList();
+    return View(contatos);
 }
 
 }
