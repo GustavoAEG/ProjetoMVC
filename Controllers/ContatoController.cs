81bbe0eef6697e069625fcb0d93b7987b232fbaa
Gustavo Enricco Goncalves Alexandre <gustavoalexandre@employer.com.br>
2023-01-19 18:43:58 -0300

    #010 - Criar contato Get e Post

--- Controllers/ContatoController.cs
+++ Controllers/ContatoController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using ProjetoMVC.Context;
+using ProjetoMVC.Models;
 
 namespace ProjetoMVC.Controllers;
 public class ContatoController : Controller{
@@ -14,6 +15,27 @@ public IActionResult Index()
 {
     var contatos = _context.Contatos.ToList();
     return View(contatos);
+}
+
+//httpget opcional...
+//chhama a pagina CRIAR CONTATO
+//GET Quando entro na página
+public ActionResult Criar(){
+
+    return View();
+}
+
+//Post quando estou na pagina e envio informações pra salvar
+[HttpPost]
+public IActionResult Criar(Contato contato){
+
+if(ModelState.IsValid){// se os dados estiverem validos
+    
+    _context.Contatos.Add(contato);
+    _context.SaveChanges();
+    return RedirectToAction(nameof(Index));
+}
+return View(contato);
 }
 
 }
