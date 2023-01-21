12128a7052e2136e3051f0361e9082404276fcbc
Gustavo Enricco Goncalves Alexandre <gustavoalexandre@employer.com.br>
2023-01-20 22:48:27 -0300

    #014 - Método Post HttpPost Edição de contato.

--- Controllers/ContatoController.cs
+++ Controllers/ContatoController.cs
@@ -28,7 +28,8 @@ public ActionResult Criar(){
 
 //Post quando estou na pagina e envio informações pra salvar
 [HttpPost]
-public IActionResult Criar(Contato contato){
+public IActionResult Criar(Contato contato)
+{
  
     _context.Contatos.Add(contato);
 
@@ -44,15 +45,31 @@ public IActionResult Criar(Contato contato){
     return View(contato);
 }
 
-public IActionResult Editar(int id){
-
+public IActionResult Editar(int id)
+{
     var contato = _context.Contatos.Find(id);
 
     if(contato == null)
 
-    return RedirectToAction(nameof(Index));
+    return RedirectToAction(nameof(Index));//Caso não existe retorna à lista.
 
     return View(contato); //caso exista retorna os valores no campo
 }
 
+[HttpPost]
+public IActionResult Editar(Contato contato)
+{
+var contatoBanco = _context.Contatos.Find(contato.Id);
+//ContatoBanco recebe o Contato que acaba de receber na página do front
+contatoBanco.Nome = contato.Nome;
+contatoBanco.Telefone = contato.Telefone;
+contatoBanco.Ativo = contato.Ativo;
+
+_context.Contatos.Update(contatoBanco);
+_context.SaveChanges();
+
+return RedirectToAction(nameof(Index));
+
 }
+}
+
